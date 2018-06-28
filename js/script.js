5f56316bf404219d3f821ac5997acd3d44f74f5e
Maria Grigorova <mmgrigorova@gmail.com>
2018-06-28 16:21:22 +0300

    added script

--- js/script.js
+++ js/script.js
@@ -1,110 +1,125 @@
-$(document).ready(function () {
-    var endPoint = 'https://api.punkapi.com/v2/';
-    // var query = 'beers?page=1&per_page=5';
-    var query = 'beers';
-    var url = new URL(endPoint + query);
+var endPoint = 'https://api.punkapi.com/v2/';
+// var query = 'beers?page=1&per_page=5';
+var query = 'beers';
+var url = new URL(endPoint + query);
 
-    var beers = [];
-    var foodPair = 'This beer will go perfectly with:';
+var beers = [];
+var foodPair = 'This beer will go perfectly with:';
 
-    $.getJSON(url, function (data) {
-        beers.push(data);
-        console.log('success');
-        readBeers(beers);
-    }, "jsonp");
+$.getJSON(url, function (data) {
+    beers.push(data);
+    console.log('success');
+    readBeers(beers);
+}, "jsonp");
 
-    var populateBeerContainer = function (beer) {
-        $("#beers-container").append(
-            $("<article></article>")
-            .addClass("beer-box")
-            .attr("id", beer.id)
-        );
+var populateBeerContainer = function (beer) {
+    $("#beers-container").append(
+        $("<article></article>")
+        .addClass("beer-box")
+        .attr("id", beer.id)
+    );
 
-        var $article = $(".beer-box#" + beer.id);
+    var $article = $(".beer-box#" + beer.id);
 
-        $article.append(
-            $("<h1></h1>")
-            .addClass("beer-name")
-            .html(beer.name)
-        );
-        $article.append($("<img />")
-            .attr("src", beer.image_url)
-            .attr("alt", beer.name)
-        );
+    $article.append(
+        $("<h1></h1>")
+        .addClass("beer-name")
+        .html(beer.name)
+    );
+    $article.append($("<img />")
+        .attr("src", beer.image_url)
+        .attr("alt", beer.name)
+    );
 
-        $article.append($("<h2></h2>")
-            .html(beer.tagline)
-        );
+    $article.append($("<h2></h2>")
+        .html(beer.tagline)
+    );
 
-        $article.append(
-            $("<div></div>")
-            .addClass("beer-abv")
-            .html("ABV: " + beer.abv)
-        );
+    $article.append(
+        $("<div></div>")
+        .addClass("beer-abv")
+        .html("ABV: " + beer.abv)
+    );
 
-        $article.append(
-            $("<button></button>")
-            .addClass("more-info")
-            .html("more info")
-        );
+    $article.append(
+        $("<a></a>")
+        .addClass("more-info")
+        .attr("href", "")
+        .html("more info")
+    );
 
-        $article.append(
-            $("<div></div>")
-            .addClass("beer-details")
-        );
+    $article.append(
+        $("<div></div>")
+        .addClass("beer-details")
+    );
 
-        var $details = $("#" + beer.id + " .beer-details");
-        $details.append(
-            $("<p></p>")
-            .addClass("beer-description")
-            .html(beer.description)
-        );
+    var $details = $("#" + beer.id + " .beer-details");
+    $details.append(
+        $("<p></p>")
+        .addClass("beer-description")
+        .html(beer.description)
+    );
 
-        $details.append(
-            $("<p></p>")
-            .addClass("beer-tips")
-            .html(beer.description)
-        );
+    $details.append(
+        $("<p></p>")
+        .addClass("beer-tips")
+        .html(beer.description)
+    );
 
-        $details.append(
-            $("<p></p>")
-            .addClass("food-pair")
-            .html(foodPair)
-        );
+    $details.append(
+        $("<p></p>")
+        .addClass("food-pair")
+        .html(foodPair)
+    );
 
-        $details.append(
-            $("<ul></ul>")
-            .addClass("food-pair")
-            .html(function () {
-                beer.food_pairing.forEach(element => {
-                    $("<li></li>")
-                        .html(element)
-                        .appendTo("#" + beer.id + " .food-pair")
-                });
-            })
-        );
-    }
+    $details.append(
+        $("<ul></ul>")
+        .addClass("food-pair")
+        .html(function () {
+            beer.food_pairing.forEach(element => {
+                $("<li></li>")
+                    .html(element)
+                    .appendTo("#" + beer.id + " .food-pair")
+            });
+        })
+    );
+}
 
-    function readBeers(beers) {
-        if (beers) {
-            // console.log(beers);
-            for (var i = 0; i < beers[0].length; i++) {
-                var beer = beers[0][i];
-                // console.log(beer.name);
-                populateBeerContainer(beer);
-            }
+function readBeers(beers) {
+    if (beers) {
+        // console.log(beers);
+        for (var i = 0; i < beers[0].length; i++) {
+            var beer = beers[0][i];
+            // console.log(beer.name);
+            populateBeerContainer(beer);
         }
-    };
+    }
+};
 
+$(document).ready(function () {
+   
     //
     // Modal Handler
     //
     var $modal = $(".modal-background");
     var $content = $(".modal-content");
 
-    var populateModal = function(beerid){
+    var getBeer = function (beerid) {
+        for (var i = 0; i < beers[0].length; i++) {
+            var beer = beers[0][i];
+            console.log(beerid);
+            console.log(beer);
+            if (beer.id == beerid) {
+                return beer;
+            }
+        }
+    }
+
+    var populateModal = function (beerid) {
         $modal.attr("id", beerid);
-        $(".beer-name").html(beer.name);
+        var beerItem = getBeer(beerid);
+        console.log(beerItem);
+        $(".beer-name").html(beerItem.name);
     }
 
 
@@ -116,7 +131,9 @@ $(document).ready(function () {
 
     $(".more-info").on("click", function () {
         var $beerid = $(this).parent().attr("id");
-        populateModal($beerid);
+        e.preventDefault();
+        console.log($beerid);
+        // populateModal($beerid);
         openModal($beerid);
     });
 
